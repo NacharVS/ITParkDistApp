e789f4e908e66e8cd1c1251131c7473b9dc0f1c0
Andrei <Bitolborg@yandex.ru>
2021-12-03 21:28:46 +0300

    MyFirstCommitInAcademy

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine ("Всем привет!");
+            Console.WriteLine ("Всем привет");
             Console.ReadKey();
         }
     }
