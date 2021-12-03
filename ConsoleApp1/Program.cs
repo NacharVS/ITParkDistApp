7a80d95977c9ee3572533a0688043b204b566fec
Nelya777 <naila201520@gmail.com>
2021-12-04 02:10:07 +0300

    add inscription

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Beep(220, 1000);         
+            Console.Beep(220, 1000);
+            Console.WriteLine("Hello friends");
         }
     }
 }
