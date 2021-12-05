19ec03839803003bcc78734f688ee85e8986e8ea
Olga Putrova <olgaputrova@gmail.com>
2021-12-05 19:50:07 +0300

    Seasons

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
         {
             //double footDistance = double.Parse(Console.ReadLine()); //0.305 metr
             //double metrDistance = double.Parse(Console.ReadLine());
-            
+            /*
             Console.WriteLine( "enter Speed meter/sec");
             double msSpeed = double.Parse(Console.ReadLine());
             Console.WriteLine("enter Speed km/h");
@@ -21,6 +21,41 @@ namespace ConsoleApp1
             else
             {
                 Console.WriteLine($"max Speed is:{kmhSpeed}  km/h");
+            }
+            */
+
+            /*
+            string s = Console.ReadLine();
+            s = s.ToLower();
+            switch (s)
+            {
+                case "monday": Console.WriteLine(1);
+                    break;
+                case "tuesday": Console.WriteLine(2);
+                    break;
+                case "wednesday": Console.WriteLine(3);
+                    break;
+                case "thursday": Console.WriteLine(4);
+                    break;
+                default : Console.WriteLine("incorrect input");
+                    break;
+            }
+            */
+            Console.WriteLine("enter season ");
+            string season = Console.ReadLine();
+            season = season.ToLower();
+            switch (season)
+            {
+                case "spring": Console.WriteLine(" - March, Aprile, May");
+                    break;
+                case "summer": Console.WriteLine(" - June, July, August");
+                    break;
+                case "autumn": Console.WriteLine(" - September, October, November");
+                    break;
+                case "winter": Console.WriteLine(" - December, January, February");
+                    break;
+                default : Console.WriteLine("incorrect input");
+                    break;
             }
             
         }
