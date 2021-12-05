638e339d6a3737793de0d2b2f76529e7ad7ec464
Olga Putrova <olgaputrova@gmail.com>
2021-12-05 19:23:11 +0300

    Speed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,19 +6,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            double footDistance = double.Parse(Console.ReadLine()); //0.305 metr
-            double metrDistance = double.Parse(Console.ReadLine());
-
-
-            if (footDistance * 0.305 > metrDistance)
+            //double footDistance = double.Parse(Console.ReadLine()); //0.305 metr
+            //double metrDistance = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine( "enter Speed meter/sec");
+            double msSpeed = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter Speed km/h");
+            double kmhSpeed = double.Parse(Console.ReadLine()); // *1000/3600
+            
+            if (msSpeed > kmhSpeed * 1000 / 3600)
             {
-                Console.WriteLine ("max is:");
-                Console.WriteLine (footDistance * 0.305);
+                Console.WriteLine ( $"max Speed is:{msSpeed}  meter/sec");
             }
             else
-            { 
-                Console.Write ("max is: ");
-                Console.Write (metrDistance);
+            {
+                Console.WriteLine($"max Speed is:{kmhSpeed}  km/h");
             }
             
         }
