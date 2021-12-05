eeec529cedb41a7336b12eb481605c1dd267ea4f
Petrikovnl <petrikovnl@gmail.com>
2021-12-05 14:58:03 +0500

    MySecondCommitInAcademy

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            var text = Console.ReadLine();
+            Console.WriteLine(text);
             Console.WriteLine ("Hello, world!"); //my comment!
             Console.WriteLine("Hello, C#!"); //next comment
         }
