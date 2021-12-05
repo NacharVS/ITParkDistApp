ebbd147ba76f6ac835e95d73d731c4ebf1503e11
Petrikovnl <petrikovnl@gmail.com>
2021-12-05 20:39:35 +0500

    IF

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var text = Console.ReadLine();
-            Console.WriteLine(text);
-            Console.WriteLine ("Hello, world!"); //my comment!
-            Console.WriteLine("Hello, C#!"); //next comment
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if (a > b && a > c)
+                Console.WriteLine("Nubmer Max = " + a);
+            else if (b > c)
+                Console.WriteLine("Number Max = " + b);
+            else
+                Console.WriteLine("Number Max = " + c);
+
+            Console.ReadLine();
+
         }
     }
 }
