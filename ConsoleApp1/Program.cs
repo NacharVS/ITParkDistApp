3b1f71444c8bfdca89d6975a5eb4bd8d159de672
Petrikovnl <petrikovnl@gmail.com>
2021-12-05 21:11:54 +0500

    speedExample

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-
+using System.Globalization;
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            /* 
+            //1 задача:
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
             int c = int.Parse(Console.ReadLine());
@@ -15,9 +17,26 @@ namespace ConsoleApp1
                 Console.WriteLine("Number Max = " + b);
             else
                 Console.WriteLine("Number Max = " + c);
-
+            //2 задача:
+            Console.WriteLine("Insert in feets:");
+            double feetDistance = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture); //0.305 meter
+            Console.WriteLine("Insert in meters:");
+            double meterDistance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double exchange = feetDistance * 0.305;
+            if (exchange > meterDistance)
+                Console.WriteLine("More is:" + feetDistance);
+            else Console.WriteLine("More is:" + meterDistance);
+            Console.ReadLine();*/
+            //3 задача:
+            Console.WriteLine("Insert in km/h:");
+            double kmspeed = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //0.305 meter
+            Console.WriteLine("Insert in m/s:");
+            double mspeed = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double exchange = kmspeed * 0.277778;
+            if (exchange > mspeed)
+                Console.WriteLine("More is:" + kmspeed);
+            else Console.WriteLine("More is:" + mspeed);
             Console.ReadLine();
-
         }
     }
 }
