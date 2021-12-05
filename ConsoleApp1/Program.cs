bdc7074128c654fca95aca859ae44afead4bfcea
DESKTOP-F8PTRBK\Нияз <kamalov.niaz@mail.ru>
2021-12-05 20:39:16 +0300

    Этап второй

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,9 +6,36 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine ("Hello, world!");
-            Console.WriteLine("Hello, C#!");
-            
+            string s = Console.ReadLine(); // winter --> december, january, february
+            s = s.ToLower();
+            switch (s)
+            {
+                case "monday":
+                    Console.WriteLine(1);
+                    break;
+                case "tuesday":
+                    Console.WriteLine(2);
+                    break;
+                case "wednesday":
+                    Console.WriteLine(3);
+                    break;
+                case "thursday":
+                    Console.WriteLine(4);
+                    break;
+                case "friday":
+                    Console.WriteLine(5);
+                    break;
+                case "saturday":
+                    Console.WriteLine(6);
+                    break;
+                case "sunday":
+                    Console.WriteLine(7);
+                    break;
+                default:
+                    Console.WriteLine("incorrect input");
+                    break;
+            }
+
         }
     }
 }
