e723c368f0d2dfac021f267019c201522dcb71bf
Petrikovnl <petrikovnl@gmail.com>
2021-12-05 21:43:55 +0500

    season

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -28,7 +28,7 @@ namespace ConsoleApp1
             else Console.WriteLine("More is:" + meterDistance);
             Console.ReadLine();*/
             //3 задача:
-            Console.WriteLine("Insert in km/h:");
+            /*Console.WriteLine("Insert in km/h:");
             double kmspeed = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //0.305 meter
             Console.WriteLine("Insert in m/s:");
             double mspeed = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
@@ -36,7 +36,35 @@ namespace ConsoleApp1
             if (exchange > mspeed)
                 Console.WriteLine("More is:" + kmspeed);
             else Console.WriteLine("More is:" + mspeed);
-            Console.ReadLine();
+            Console.ReadLine();*/
+            Console.WriteLine("Input the season:");
+            string s = Console.ReadLine();
+            switch(s)
+            {
+                case "winter":Console.WriteLine("december,january,february");
+                    break;
+                case "summer":
+                    Console.WriteLine("june,july,august");
+                    break;
+                case "spring":
+                    Console.WriteLine("march,may,april");
+                    break;
+                case "autumn":
+                    Console.WriteLine("september,october,november");
+                    break;
+                /*case "friday":
+                    Console.WriteLine(5);
+                    break;
+                case "saturday":
+                    Console.WriteLine(6);
+                    break;
+                case "sunday":
+                    Console.WriteLine(7);
+                    break;*/
+                default:Console.WriteLine("incorrect season");
+                    break;
+            }
+
         }
     }
 }
