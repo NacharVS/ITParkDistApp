048badd4d6ab50a118a45ab879edd75f1fcc5411
nacharvs <r3dinc@yandex.ru>
2021-12-05 21:08:07 +0300

    shop

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,34 +6,31 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string s = Console.ReadLine(); // winter --> december, january, february
-            s = s.ToLower();
-            switch (s)
+            Console.WriteLine("Put Ur money in terminal");
+            int moneyCount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Products:");
+            Console.WriteLine("1. Bread - 50");
+            Console.WriteLine("2. Milk - 40");
+            Console.WriteLine("3. Butter - 70");
+            Console.WriteLine("4. Chocolate - 110");
+            Console.WriteLine("5. Coffee - 100");
+            Console.Write("Enter the number of product:");
+            int productNumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter the count of product:");
+            int productCount = int.Parse(Console.ReadLine());
+
+            switch (productNumber)
             {
-                case "monday":
-                    Console.WriteLine(1);
+                case 1: if(moneyCount < productCount * 50)
+                        Console.WriteLine("Not enouth money");
+                else
+                        Console.WriteLine($"U have bought {productCount} of bread with {productCount * 50} price, Urchange {moneyCount - productCount * 50}");
                     break;
-                case "tuesday":
-                    Console.WriteLine(2);
-                    break;
-                case "wednesday":
-                    Console.WriteLine(3);
-                    break;
-                case "thursday":
-                    Console.WriteLine(4);
-                    break;
-                case "friday":
-                    Console.WriteLine(5);
-                    break;
-                case "saturday":
-                    Console.WriteLine(6);
-                    break;
-                case "sunday":
-                    Console.WriteLine(7);
-                    break;
-                default: Console.WriteLine("incorrect input");
+                default:
+                    Console.WriteLine("Incorrect input");
                     break;
             }
+        }
             //Max distance in foot/meters is MAX
         }
     }
