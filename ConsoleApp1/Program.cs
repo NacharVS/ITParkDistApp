c9aedcfd28495fc08f10bfd865a2e0503d2671e9
Pinklelik <Bokovikova.92@mail.ru>
2021-12-06 02:48:36 +0300

    fd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello, C#!");
             Console.WriteLine("Hello, every body!");
             Console.WriteLine("Alohomora");
+
         }
     }
 }
