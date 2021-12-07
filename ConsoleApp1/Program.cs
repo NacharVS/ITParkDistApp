4452727f58c822a71f8e81300218bdf6fcc89213
akhmadullinaynur <akhmadullinaynur@yandex.ru>
2021-12-07 22:59:31 +0300

    Homework 1.3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,39 +6,59 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int number = int.Parse(Console.ReadLine());
-            int ed = number % 10;
-            int des = number / 10 % 10;
-            int hun = number / 100;
-                if (ed == 0)
-                Console.WriteLine(ed * des * hun);
-            else
-                if (ed==2)
-                Console.WriteLine(ed * des * hun);
-            else
-                if (ed == 4)
-                Console.WriteLine(ed * des * hun);
-            else
-                if (ed == 6)
-                Console.WriteLine(ed * des * hun);
-            else
-                if (ed == 8)
-                Console.WriteLine(ed * des * hun);
-            else
-                if (ed == 1)
-                Console.WriteLine(ed + des + hun);
-            else
-                if (ed == 3)
-                Console.WriteLine(ed + des + hun);
-            else
-                if (ed == 5)
-                Console.WriteLine(ed + des + hun);
-            else
-                if (ed == 7)
-                Console.WriteLine(ed + des + hun);
-            else
-                if (ed == 9)
-                Console.WriteLine(ed + des + hun);
+            Console.WriteLine("Выберите фигуру:");
+            Console.WriteLine("1. Квадрат");
+            Console.WriteLine("2. Прямоугольник");
+            Console.WriteLine("3. Круг");
+            Console.WriteLine("4. Треугольник");
+            Console.WriteLine("5. Ромб");
+            int figure = int.Parse(Console.ReadLine());
+            switch (figure)
+            { 
+                case 1:
+                    {
+                        Console.WriteLine("Введите значение стороны:");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Площадь квадрата равна: {a * a}");
+                    }
+                    break;
+                    case 2:
+                    {
+                        Console.WriteLine("Введите значение длины:");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Введите значение ширины:");
+                        double b = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Площадь прямоугольника равна: {a * b}");
+                    }
+                    break;
+                    case 3:
+                    {
+                        Console.WriteLine("Введите значение радиуса:");
+                        double r = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Площадь круга равна: {Math.PI * Math.Pow(r,2)}");
+                    }
+                    break;
+                    case 4:
+                    {
+                        Console.WriteLine("Введите значение основания:");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Введите значение высоты:");
+                        double h = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Площадь треугольника равна: {0.5 * a*h}");
+                    }
+                    break;
+                    case 5:
+                    {
+                        Console.WriteLine("Введите значение стороны:");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Введите значение высоты:");
+                        double h = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Площадь ромба равна: {a * h}");
+                    }
+                    break;
+            }
+                
+
 
         }
     }
