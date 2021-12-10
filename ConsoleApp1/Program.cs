83f3e180990b045016f930839d8536c9275bbd49
Alexey <AVProsvirnin@bk.ru>
2021-12-10 14:44:45 +0400

    Homework_3_1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,20 +6,40 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            double a = int.Parse(Console.ReadLine());
-            double b = int.Parse(Console.ReadLine());
+            //Entering the sum of three numbers.
+            Console.WriteLine("Enter the sum of numbers in the range from 3 to 24:");
+            int summ = int.Parse(Console.ReadLine());
+            
+            int combinationCount = 0;// 2 (0, 1, 2) < summ < 25 (7, 8, 9).
 
-            double resalt = 1;
-
-            for (int i = 1; i <= b; i++)
+            //Checking a summ is in the correct range.
+            if (summ > 2 && summ < 25)
             {
-                resalt = resalt * a;
+                //Matching small numbers in combination.
+                for (int i = 0; i <= 9; i++)
+                {
+                    //Matching averages numbers in combination
+                    for (int j = 0; j <= 9; j++)
+                    {
+                        //Matching larges numbers in combination.
+                        for (int k = 0; k <= 9; k++)
+                        {
+                            //Selection condition for a combination of three digits.
+                            if (i + j + k == summ && i < j && j < k)
+                            {
+                                
+                                Console.WriteLine($"{i}{j}{k}");
+                                
+                                combinationCount++;
+                            }
+                        }
+                    }
+                }
             }
 
-            Console.WriteLine(resalt);
+            else Console.WriteLine("You entered the wrong number. The number must be between 3 and 24!");
 
-
-
+            Console.WriteLine($" count {combinationCount}");
         }
 
 
