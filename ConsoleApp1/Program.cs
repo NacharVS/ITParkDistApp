467f207e95303e9c03fae384dc25894426237ca6
Alexey <AVProsvirnin@bk.ru>
2021-12-10 21:55:44 +0400

    Homework_3_3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,39 +2,42 @@
 
 namespace ConsoleApp1
 {
-    // Ввывести представление числа N в виде простых множителей. 
-    // input 6 output 2 3 
-    // intput 30 output 2 3 5
-    // input 16 output 2 2 2 2
+    // Дана числовая последовательность от 1 до N. Найти такое число сумма цифр которого будет
+    // наибольшей среди всей последовательности. Вывести само число и его сумму цифр
+    // input 20 output 19 10
+    // input 44 output 39 12
     class Program
     {
         static void Main(string[] args)
         {
 
             Console.WriteLine("Enter number N;");
-            int number = int.Parse(Console.ReadLine());
-                        
-            while (number > 1)
+            int n = int.Parse(Console.ReadLine());
+            
+            int maxNumber = 0;
+            int maxSumm = 0;
+
+            //Iterate through all digits of the sequence.
+            for (int i = 1; i <= n; i++)
             {
-                for (int i = 2; i <= number; i++)
+                int number = i;
+                int summ = 0;
+
+                //Finding the sum of the digits of the number i from the sequence.
+                while (number > 0)
                 {
-                    if (number % i == 0)
-                    {
-                        number = number / i;
-                        
-                        Console.Write($"{i} ");
-                        break;
-                    }
+                    summ = summ + number % 10;
+                    number = number / 10;
+                }
+
+                if (maxSumm < summ)
+                {
+                    maxSumm = summ;
+                    maxNumber = i;
                 }
             }
 
+            Console.WriteLine($"The number: {maxNumber} has the largest sum of digits: {maxSumm} among all the numbers in the sequence from 1 to {n}");
         }
-        
-
-        // Дана числовая последовательность от 1 до N. Найти такое число сумма цифр которого будет
-        // наибольшей среди всей последовательности. Вывести само число и его сумму цифр
-        // input 20 output 19 10
-        // input 44 output 39 12
-
     }
 }
