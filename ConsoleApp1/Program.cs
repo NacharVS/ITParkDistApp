3b21150cf31baf2141825063ddce53637b432dee
Petrikovnl <petrikovnl@gmail.com>
2021-12-11 21:51:19 +0500

    ThirdLecturePractice(3task)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-
 namespace ConsoleApp1
 {
     class Program
@@ -242,7 +241,6 @@ namespace ConsoleApp1
             for (int i = 1; i <= n; i++)
             {
                 result = result * i;
-                
             }
             Console.WriteLine(result);*/
             //-----------------------------------------------------
@@ -255,7 +253,6 @@ namespace ConsoleApp1
             {
                 depositResult = depositSum + (depositSum * 0.04);
             }
-
             Console.WriteLine($"Итоговая сумма к концу вклада: {depositResult}");*/
             //---------------------
             /*Console.Write("Введите число, a: ");
@@ -316,7 +313,6 @@ namespace ConsoleApp1
                 }
                 Console.WriteLine();
             }*/
-
             /*int summ = int.Parse(Console.ReadLine());
             for (int i = 1; i < 999; i++)
             {
@@ -325,47 +321,108 @@ namespace ConsoleApp1
                     Console.WriteLine($"{i / 100} {i / 10 % 10} {i % 10}");
                 }
             }*/
-            int combinations=0;
-            int summ = int.Parse(Console.ReadLine());
-            for (int i = 0; i <=9; i++)
-            {
-                for (int j = 0; j <= 9; j++)
-                {
-                    for (int k= 0; k <= 9; k++)
-                    {
-                        if (i + j + k == summ && i!=j && i != k  && j != k/* && j != k&& k != i&& k != j*/)
-                        {
-                            Console.WriteLine($"{i}{j}{k}");
-                            combinations++;
-                        }
-                    }
-                }
-            }
-            Console.WriteLine($"Count: {combinations}");
             //------------------------ for и while
-            /*int n = int.Parse(Console.ReadLine());
-            int count=0;
-            for (int i = 1; i <=n; i++)
+            /*          int n = int.Parse(Console.ReadLine());
+                        int count=0;
+                        for (int i = 1; i <=n; i++)
+                        {
+                            int a = i;
+                            while (a > 0)
+                            {
+                                if (a % 10 == 7)// i == 7 ||  || i / 10 == 7)
+                                    count++;
+                                a = a / 10;
+                            }
+                        }
+                        Console.WriteLine($"Count: {count}");
+            */
+            //1 задача. Нахождение комбинаций 3х-значного кодового замка по его сумме:
+            /*          
+                        Console.Write("Введите сумму цифр кодовой комбинации замка: ");    
+                        int combinations=0;
+                        int iCheck = 0;
+                        int jCheck = 0;
+                        int kCheck = 0;
+                        int summ = int.Parse(Console.ReadLine());
+                        for (int i = 0; i <= 9; i++)
+                        {
+                            if (i != iCheck&&i!=jCheck&&i!=kCheck)
+                            {
+                                for (int j = 0; j <= 9; j++)
+                                {
+                                    for (int k = 0; k <= 9; k++)
+                                    {
+                                        if (i + j + k == summ && i != j && i != k && j != k)
+                                        {
+                                            iCheck = i;
+                                            jCheck = j;
+                                            kCheck = k;
+                                            Console.WriteLine($"{i}{j}{k}");
+                                            combinations++;
+                                            if (i < 9) i++;
+                                            if (j < 9) j++;
+                                            if (k < 9) k++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        Console.WriteLine($"Всего комбинаций: {combinations}");
+            */
+            //2 задача. Простые множители.
+            /*        
+                      Console.Write("Введите число для поиска его простых множителей: ");
+                      int number = int.Parse(Console.ReadLine());
+                      Console.Write($"{number} = 1");
+                      for (int i = 0; number % 2 == 0; number /= 2)
+                      {
+                          Console.Write(" * 2");
+                      }
+                      for (int i = 3; i <= number;)
+                      {
+                          if (number % i == 0)
+                          {
+                              Console.Write($" * {i}");
+                              number /= i;
+                          }
+                          else
+                          {
+                              i += 2;
+                          }
+                      }
+          */
+            //3 задача.Нахождение максимальной суммы цифр в числовом ряде:
+            Console.Write("Введите конечное число N ряда 1...N: ");
+            int sequence = int.Parse(Console.ReadLine());
+            int numberOfMaxSum = 0;
+            int maximumSum = 0;
+            for (int i = 1; i <= sequence; i++)
             {
-                int a = i;
-                while (a > 0)
+                int numberOfSequence = i;
+                int sumOfNumber = 0;
+                for (int j = numberOfSequence; j > 0; j = j / 10)
                 {
-                    if (a % 10 == 7)// i == 7 ||  || i / 10 == 7)
-                        count++;
-                    a = a / 10;
+                    sumOfNumber += numberOfSequence % 10;
+                    numberOfSequence = numberOfSequence / 10;
+                }
+                //Console.WriteLine($"Число ряда: {i} имеет сумму цифр: {sumOfNumber}");
+                if (sumOfNumber > maximumSum)
+                {
+                    maximumSum = sumOfNumber;
+                    numberOfMaxSum = i;
                 }
             }
-            Console.WriteLine($"Count: {count}");*/
+            Console.WriteLine($"В заданном ряде (1-{sequence}) число {numberOfMaxSum} имеет максимальную сумму цифр: {maximumSum}");
         }
-
-        // сократить кол-во комбинаций
-//вывести представление числаN в в виде простых множетелей
-//input 6 output 2 3
-//input 30 output 2 3 5
-// input 16 output 2 2 2 2
-// дана числовая последовательность от 1 до N Найти такое число сумма цифр кторого будет наибольшей среди всей последовательности.
-//вывести само число и его сумму цифр
-//input (1 до n) 20 output 19 10
-//input 44 output 39 12
     }
 }
+//(V)1.Сократить кол-во комбинаций.
+//(V)2.Вывести представление числа N в виде простых множетелей.
+//      input 6 output 2 3
+//      input 30 output 2 3 5
+//      input 16 output 2 2 2 2
+//3.Дана числовая последовательность от 1 до N;
+//      Найти такое число сумма цифр которого будет наибольшей среди всей последовательности;
+//      Вывести само число и его сумму цифр.
+//      input (1 до n) 20 output 19 10
+//      input 44 output 39 12
