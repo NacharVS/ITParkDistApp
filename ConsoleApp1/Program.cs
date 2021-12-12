b3a34b97b6d846a678f44024cd60dc638309a7b0
NacharVS <r3dinc@yandex.ru>
2021-12-12 18:43:26 +0300

    Update Program.cs
    arrays

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,35 +6,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            int sum = 0;
-            for (int i = 1; i <= n; i++)
+            int[] array = new int[10];
+
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
             {
-                int a = i;
-                while(a > 0)
-                {
-                    if(a % 10 == 7)
-                    {
-                        sum++;
-                    }
-                    a = a / 10;
-                }
+                array[i] = random.Next(100);
+                Console.Write($"{array[i]} ");
             }
-            Console.WriteLine(sum);
-            // input 20  output 2
-            // input 100 output 20
+
+            Console.WriteLine();
+            // поиск элементов которые больше чем предыдущий
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if(array[i+1] > array[i])
+                    Console.Write($" {array[i + 1]}");
+            }
+
+
         }
-        // Сократить количество комбинаций, хотя бы в 2 раза. 
-
-        // Ввывести представление числа N в виде простых множителей. 
-        // input 6 output 2 3 
-        // intput 30 output 2 3 5
-        // input 16 output 2 2 2 2
-
-        // Дана числовая последовательность от 1 до N. Найти такое число сумма цифр которого будет
-        // наибольшей среди всей последовательности. Вывести само число и его сумму цифр
-        // input 20 output 19 10
-        // input 44 output 39 12
-
+        
     }
 }
