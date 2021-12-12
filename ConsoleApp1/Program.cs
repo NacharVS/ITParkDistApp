3f168a2dd56e34cad94fb03b385247bdfe355a99
AndreiZasuhin <Bitolborg@yandex.ru>
2021-12-12 18:09:43 +0300

    Mission5

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,22 +8,17 @@ namespace ConsoleApp1
         {
             int summ = int.Parse(Console.ReadLine());
             int combinations = 0;// 1 < summ < 27
-            int a = 0;
-            int b = a;
             for (int i = 0; i <= 7; i++)
-               // if (i!=1)
             {
-                for (int j = 1; j <= 8; j++)
-                    if (j!=a)
+                for (int j = i + 1; j <= 8; j++)
                 {
-                        
-                    for (int k = 2; k <= 9; k++)
+
+                    for (int k = j + 1; k <= 9; k++)
                     {
                         if (i + j + k == summ && i != j && j != k && i != k)
                         {
                             Console.WriteLine($"{i}{j}{k}");
                             combinations++;
-                            a = k;
                             break;
                         }
                     }
