8175bab563b327405a89b37799fda79dcc0535bd
daryazakharova <daryazakharova87@gmail.com>
2021-12-12 16:42:59 +0300

    HW2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,86 +7,53 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             
-           //среднее значение
+            int summ = int.Parse(Console.ReadLine());
+            int combination = 0;
 
-            Console.WriteLine("Еnter the first number: ");
-            int number1 = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int j = 0; j <= 9; j++)
+                {
+                    for (int k = 0; k <= 9; k++)
+                    {
+                        if (i + j + k == summ && i != j && j != k && i != k)
 
-            Console.WriteLine("Еnter the second number: ");
-            int number2 = int.Parse(Console.ReadLine());
+                        {
+                            if (i < j && j < k)
 
-            Console.WriteLine("Еnter the third number: ");
-            int number3 = int.Parse(Console.ReadLine());
+                                Console.WriteLine($"{i}{j}{k}");
+                            combination++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"combination: {combination}");
 
-            int mean = (number1 + number2 + number3) / 3;
 
-            Console.WriteLine($"Mean: { mean }");
+            Console.ReadLine();
+
+            //Ввывести представление числа N в виде простых множителей. 
+            // input 6 output 2 3 // intput 30 output 2 3 5 // input 16 output 2 2 2 2
+
+
+            Console.WriteLine("insert the number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int x = 2; x <= num; x++)
+            {
+                while (num % x == 0)
+                {
+                    num = num / x;
+                    Console.Write(x);
+                    
+                }
+            }
            
-
-            //трехзначное число. четное, то найти сумму цифр, если оно нечетное, найти произведение.
-
-            Console.WriteLine("Enter a three-digit number: ");
-
-            int number = int.Parse(Console.ReadLine());
-            int a = number % 10;
-            int b = number / 10 % 10;
-            int c = number / 100;
-            int d = a + b + c;
-            int e = a * b * c;
-
-            if (number % 2 == 0)
-            {
-                Console.WriteLine(d);
-            }
-            else
-            {
-                Console.WriteLine(e);
-            }
-
-            
-
-            //Написать программу, которая, в зависимости от выбора пользователя, предложит найти площадь геометрической фигуры. 
-            Console.WriteLine("Choose a figure");
-            Console.WriteLine("1. square ");
-            Console.WriteLine("2. rectangle");
-            int figure = int.Parse(Console.ReadLine());
-
-
-            if (figure == 1)
-            {
-                Console.Write("enter side length: ");
-                int lenght = int.Parse(Console.ReadLine());
-                double Ssquare = Math.Pow(lenght, 2); 
-                Console.WriteLine($"Square area = {Ssquare}");
-            }
-            else
-            {
-                Console.Write("enter side length rectangle: ");
-                int lenghtRes = int.Parse(Console.ReadLine());
-                Console.Write("enter side width rectangle: ");
-                int widthRes = int.Parse(Console.ReadLine());
-                int Srectangle = lenghtRes * widthRes;
-                Console.WriteLine($"Restangle area = {Srectangle}");
-            }
-            
-
-            
-
-            //Написать программу для вычисления процентов по вкладу (ставка составляет 4% в месяц). 
-
-            Console.Write("Calculation of interest on a deposit. With a rate of 4% per month.");
-            Console.WriteLine();
-            Console.Write("Enter the initial deposit: ");
-            double deposit = int.Parse(Console.ReadLine());
-            Console.Write("Term of the contract in months:  ");
-            int numberMonths = int.Parse(Console.ReadLine());
-            double bid = 1 + 0.04;
-            double initialDeposit = deposit * Math.Pow(bid, numberMonths);
-            Console.Write($"Total income:  {initialDeposit}"); 
-            
-
-    
-
         }
     }
 }
+  
+
+   
+    
+
