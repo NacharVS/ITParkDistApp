8029c05b2bdc0f55611002e97ecc3919890e0545
DESKTOP-U06RGGG\Alberttos <albertto09@bk.ru>
2021-12-13 22:17:31 +0300

    Домашнее задание №2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,126 +6,55 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {    //Задание № 1.
-             //int a, b, c;
+        {
+            //    Задача №1
 
-            //Console.WriteLine("Введите число 1");
-            //a = int.Parse(Console.ReadLine());
+            int sum = int.Parse(Console.ReadLine());
+            int combin = 0;
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int k = 0; k <= 9; k++)
+                {
+                    for (int l = 0; l <= 9; l++)
+                    {
+                        if (i + k + l == sum && i != k && k != l && i != l)
+                        {
+                            Console.WriteLine($"{i}{k}{l}");
+                            combin++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Комбинаций=" + combin);
 
+            //---------------------------------------------------------------------------------------
+            //Задача № 3
 
-            //Console.WriteLine("Введите число 2");
-            //b = int.Parse(Console.ReadLine());
-
-            //Console.WriteLine("Введите  число 3");
-            //c = int.Parse(Console.ReadLine());
-
-            //int result = a;
-
-            //if ((a < b && b < c) || (a > b && b > c))
+            //int n = int.Parse(Console.ReadLine());
+            //int max = 0;
+            //int maxsum = 0;
+            //for (int i = 1; i <=n; i++)
             //{
-            //    result = b;
-            //}
-            //else if ((b < c && c < a) || (b > c && c > a))
-            //{
-            //    result = c;
-            //}
-            //Console.WriteLine($"Среднее число   {result}");
+            //    int a = i;
+            //    int sum = 0;
+            //    while (a>0)
+            //    {
+            //        sum += a % 10;
 
-            //----------------------------------------------------------------------
-
-            //Задание № 2.
-
-            //int summa;
-            //int a;
-            //int b;
-            //int c;
-            //int result;
-            //int result2;
-            //Console.WriteLine("Введите трехзначное число");
-            //summa = int.Parse(Console.ReadLine());
-
-
-            //a = summa / 100;
-            //b = (summa % 100) / 10;
-            //c = summa % 10;
-            //if (summa % 2 == 0)
-            //{
-            //    Console.WriteLine("Вы ввели четное число");
-            //    result = a + b + c;
-
-            //    Console.WriteLine("Cумма трех чисел = " + result);
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Число не четное");
-            //    result2 = a * b * c;
-            //    Console.WriteLine("Произведение = "+result2);
+            //        a = a / 10;
+            //    }
+            //    if (maxsum < sum)
+            //    {
+            //        maxsum = sum;
+            //        max = i;
+            //    }
             //}
 
-            //----------------------------------------------------------------------
+            //Console.WriteLine($"Число{max}сумма {maxsum}");
 
-            //Задание № 3.
-            //int Radius;
-            //int side;
-            //double footing;
-            //double height;
+            //-------------------------------------------------------------------------------------------------
 
-            //double S;
-
-
-            //Console.WriteLine("Выбирите геометрическую фигуру:круг,квадрат,треугольник");
-            //string figure = Console.ReadLine();
-            //switch (figure)
-            //{
-            //    case "круг":
-            //        Console.WriteLine("Выбирите радиус круга в см.");
-            //        Radius = int.Parse(Console.ReadLine());
-            //        S = Radius * 2;
-            //        Console.WriteLine("Площадь круга = " + S);
-            //        break;
-
-            //    case "квадрат":
-            //        Console.WriteLine("Выбирите одну сторону квадрата в см.");
-            //        side = int.Parse(Console.ReadLine());
-            //        S = side * side;
-            //        Console.WriteLine("Площадь квадрата= " + S);
-            //        break;
-
-            //    case "треугольник":
-            //        Console.WriteLine("введите основание треугольника в см.");
-            //        footing = double.Parse(Console.ReadLine());
-
-
-            //        Console.WriteLine("Введите высоту треугольника в см.");
-            //        height = double.Parse(Console.ReadLine());
-
-            //        S = footing * height/2;
-
-
-
-            //        Console.WriteLine("Площадь треугольника= " + S);
-            //        break;
-            //}
-
-            //----------------------------------------------------------------------
-
-            //Задание № 4.
-            double money = 100; // Деньги 
-            int month = 6; // Месяцы
-            double q = 3; // Процент по вкладу "3%"
-            Console.WriteLine("Введите сумму вклада");
-            money = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введите количество дней");
-            month = int.Parse(Console.ReadLine());
-            
-            double income = q / 100 + 1; // Переводим проценты в число
-
-
-            for (int i = 0; i < month; i++)
-                money *= income;
-
-            Console.WriteLine("Итого"+money);
+            //Задача №2
 
 
 
@@ -133,5 +62,12 @@ namespace ConsoleApp1
 
 
         }
+
+
+
+
+
+
     }
+    
 }
