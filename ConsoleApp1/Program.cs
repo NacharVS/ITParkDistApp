3bfcccf2c73816b9b76d5ed29888b9283050bc16
Pinklelik <Bokovikova.92@mail.ru>
2021-12-14 05:46:01 +0300

    Home work 1.2 сумма/ произведение

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,19 +6,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("введите три налуральных числа:");
-            int a = int.Parse(Console.ReadLine());
-            int b = int.Parse(Console.ReadLine());
-            int c = int.Parse(Console.ReadLine());
-            if (a > b & a > c)
-                Console.WriteLine("среднее число:" +c);
+            Console.WriteLine("Ведите трехзначное число:");
+            int number= int.Parse(Console.ReadLine());
+            int ed = number % 10;
+            int des = number / 10 % 10;
+            int hun = number / 100;
+            if (number % 2 <= 0)
+                Console.WriteLine($"четное. Сумма цифр:{ed + des + hun}");
             else
-                if (b > a & b < c)
-                Console.WriteLine("среднее число:" +b);
-            else
-                if (c > b & c < a)
-                Console.WriteLine("среднее число:" +c);
-            //
+                Console.WriteLine($"нечетное. Произведение цифр:{ed * des * hun}");
+            
+
+           
 
 
                 
