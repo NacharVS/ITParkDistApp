534507bca0dea5be74de552869f795d5ea75d5ce
nacharvs <r3dinc@yandex.ru>
2021-12-16 19:53:20 +0300

    простые множители

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,28 +8,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int>();
-            list.Add(5);
-            list.Add(323);
-            list.Add(2); 
-            list.Add(432);
-            list.Add(342); 
-            list.Add(6556);
-            list.Add(222);
-            list.Add(43);
-            if(list.Contains(222))
-                Console.WriteLine(list.IndexOf(222));
-            else
+            // найти простые множители
+            int number = int.Parse(Console.ReadLine());
+            int divider = 2;
+            while (number > 1)
             {
-                Console.WriteLine("not found");
+                for (int i = divider; i <= number; i++)
+                {
+                    if (number % divider == 0)
+                    {
+                        number = number / divider;
+                        Console.WriteLine(divider);
+                    }
+                    else
+                        divider++;
+                }
             }
 
-            Console.WriteLine("updated");
 
-            // Создать каталог товаров(Наименование и цена за единицу). 
-            // Должна быть возможность добавления новых товаров. (admin) и удаление. 
-            // Покупка нескольких товаров(ввод наименования и количества товара). Организовать подсчет общей стоимости 
-            //покупки, проверку на наличие необходимых финансов. Если покупка прошла, то вывести полный список купленных товаров и их стоимость.
            
 
         }
