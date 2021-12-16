8d0587bff24cc3e8f8ac4d4ac9a2ce36b578f28f
nacharvs <r3dinc@yandex.ru>
2021-12-16 20:29:00 +0300

    shop draft

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,27 +8,70 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // найти простые множители
-            int number = int.Parse(Console.ReadLine());
-            int divider = 2;
-            while (number > 1)
+            List<string> products = new List<string>() { "Bread", "Milk" };
+            List<double> prices = new List<double>() { 50, 50 };
+
+            List<double> cart = new List<double>() { 50, 50 };
+            Console.WriteLine("Client/Admin");
+            string s = Console.ReadLine();
+            if (s == "c")
             {
-                for (int i = divider; i <= number; i++)
+                Console.WriteLine("Enter urmoney");
+                double money = 2000;
+                while (true)
                 {
-                    if (number % divider == 0)
+                    Console.WriteLine("Our list:");
+                    for (int i = 0; i < products.Count; i++)
                     {
-                        number = number / divider;
-                        Console.WriteLine(divider);
+                        Console.WriteLine($"{i + 1}. {products[i]} {prices[i]}");
                     }
-                    else
-                        divider++;
+                    Console.Write("make Ur choice(1, 2, 3...):");
+                    string clientsChoice = Console.ReadLine();
+                    switch (clientsChoice)
+                    {
+                        case "1":
+                            {
+                                Console.Write("Enter count of Bread:");
+                                int countOfProduct = int.Parse(Console.ReadLine());
+                                cart.Add(prices[0] * countOfProduct);
+                            }
+                            break;
+                    }
+                    Console.WriteLine("something else?(Y/N)");
+                    clientsChoice = Console.ReadLine();
+                    if (clientsChoice== "N")
+                    {
+                        break;
+                    }
                 }
+                double summ = 0;
+                for (int i = 0; i < cart.Count; i++)
+                {
+                    summ += cart[i];
+                }
+                Console.WriteLine(summ);
+
+            }
+            else
+            {
+                Console.WriteLine("admin");
             }
 
 
-           
+            //Console.WriteLine("Our list:");
+            //for (int i = 0; i < products.Count; i++)
+            //{
+            //    Console.WriteLine($"{i+1}. {products[i]} {prices[i]}");
+            //}
+            //double money = 2000;
+
+
+
+
+
+
 
         }
-        
+
     }
 }
