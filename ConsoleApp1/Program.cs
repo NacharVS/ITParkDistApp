983b1da314a619f3b08c6a03f2525eaf892b562a
daryazakharova <daryazakharova87@gmail.com>
2021-12-17 13:07:57 +0300

    HW3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
@@ -6,51 +7,135 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
-            int summ = int.Parse(Console.ReadLine());
-            int combination = 0;
-
-            for (int i = 0; i <= 9; i++)
+            Console.WriteLine("Welcome to our store!");
+            List<string> products = new List<string>() {"Apples", "Yogurt", "Tomatoes", "Cucumbers", "Loaf" };
+            List<double> prices = new List<double>() {100, 50, 150, 100, 50};
+            List<double> cart = new List<double>() { 100, 50, 150, 100, 50 };
+            Console.WriteLine("log in as an administrator or as a customer? 0/1 ");
+            int choice = int.Parse(Console.ReadLine());
+            if (choice == 1)
             {
-                for (int j = 0; j <= 9; j++)
+                Console.WriteLine("Greetings, client.");
+                Console.Write("Enter the number of currency units: ");
+                double limit = double.Parse(Console.ReadLine());
+                while (true)
                 {
-                    for (int k = 0; k <= 9; k++)
+                    Console.WriteLine("Our list: ");
+                    for (int i = 0; i < products.Count; i++)
                     {
-                        if (i + j + k == summ && i != j && j != k && i != k)
-
-                        {
-                            if (i < j && j < k)
-
-                                Console.WriteLine($"{i}{j}{k}");
-                            combination++;
-                        }
+                        Console.WriteLine($"{i + 1}. {products[i]} {prices[i]}");
                     }
+                    Console.Write("Make Ur choice (1, 2, 3...): ");
+                    string clientsChoice = Console.ReadLine();
+                    switch (clientsChoice)
+                    {
+
+                        case "1":
+                            {
+                                Console.Write("Enter count of Apples:");
+                                int countOfProduct = int.Parse(Console.ReadLine());
+                                cart.Add(prices[0] * countOfProduct);
+                                
+                            }
+                            break;
+                        case "2":
+                            {
+                                Console.Write("Enter count of Yogurt:");
+                                int countOfProduct = int.Parse(Console.ReadLine());
+                                cart.Add(prices[1] * countOfProduct);
+                            }
+                            break;
+                        case "3":
+                            {
+                                Console.Write("Enter count of Tomatoes:");
+                                int countOfProduct = int.Parse(Console.ReadLine());
+                                cart.Add(prices[2] * countOfProduct);
+                            }
+                            break;
+                        case "4":
+                            {
+                                Console.Write("Enter count of Cucumbers:");
+                                int countOfProduct = int.Parse(Console.ReadLine());
+                                cart.Add(prices[3] * countOfProduct);
+                            }
+                            break;
+                        case "5":
+                            {
+                                Console.Write("Enter count of Loaf:");
+                                int countOfProduct = int.Parse(Console.ReadLine());
+                                cart.Add(prices[4] * countOfProduct);
+                            }
+                            break;
+                           
+                     }
+           Console.WriteLine("Something else?(Y/N)");
+           clientsChoice = Console.ReadLine();
+           clientsChoice = clientsChoice.ToLower();
+            if (clientsChoice == "N")
+                    {
+                        break;
+                    }
+
                 }
-            }
-            Console.WriteLine($"combination: {combination}");
-
-
-            Console.ReadLine();
-
-            //Ввывести представление числа N в виде простых множителей. 
-            // input 6 output 2 3 // intput 30 output 2 3 5 // input 16 output 2 2 2 2
-
-
-            Console.WriteLine("insert the number: ");
-            int num = int.Parse(Console.ReadLine());
-
-            for (int x = 2; x <= num; x++)
-            {
-                while (num % x == 0)
+                double summ = 0;
+                for (int i = 0; i < cart.Count; i++)
                 {
-                    num = num / x;
-                    Console.Write(x);
-                    
+                    summ += cart[i];
                 }
+                Console.WriteLine(summ);
+                if (limit< summ)
+                    Console.WriteLine("You don't have enough funds!");
+                else
+                {
+                   double change= limit - summ;
+                    Console.WriteLine($"You change: {change}");
+                }
+
             }
-           
+            else
+            {
+                Console.WriteLine("Greetings, administrator.");
+                while (true)
+                {
+                    Console.WriteLine("Do you want to add a new torav? or Delete. Add/Delete ");
+                    string choice2 = Console.ReadLine();
+                    choice2 = choice2.ToLower();
+                    switch (choice2)
+                    {
+                        case "delete":
+                            Console.WriteLine("Select the product that you want to remove from the list");
+                            products.Remove(Console.ReadLine());
+                            Console.WriteLine();
+                            break;
+
+                        case "add":
+
+                            Console.WriteLine();
+                            Console.Write("Enter a new product: ");
+                            products.Add(Console.ReadLine());
+                            Console.Write("Enter the cost of the product: ");
+                            prices.Add(double.Parse(Console.ReadLine()));
+                            Console.WriteLine();
+                            break;
+                    }
+                   
+                    Console.WriteLine("Do you want to continue working with the catalog? (Y/N)");
+                   string choice3 = Console.ReadLine();
+                    choice3 = choice3.ToLower();
+                    if (choice3 == "N")
+                    {
+                        break;
+                    }
+
+                }
+              
+            }
+
         }
-    }
+           
+            
+     }
+
 }
   
 
