3be82da533df190ed598ce042f5817dfcb540c3b
Alexey <AVProsvirnin@bk.ru>
2021-12-17 18:45:57 +0400

    Homework_5_1v2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,34 +14,54 @@ namespace ConsoleApp1
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
                     array[i, j] = rnd.Next(0, 10);
-                    Console.Write($"{array[i,j]}\t");
+                    Console.Write($"{array[i, j]}\t");
                 }
                 Console.WriteLine();
-            }    
-        } 
+            }
+        }
 
-        static void ArraySumm(int[,] arraySizeMore, int[,] arraySizeLess, int[,] arraySumm)
+        static int[,] ArraySumm(int[,] array1, int[,] array2)
         {
+            int[,] arraySum;
+            int[,] arraySizeMore;
+            int[,] arraySizeLess;
+
             Console.WriteLine();
-            
-            for (int i = 0; i < arraySumm.GetLength(0); i++)
+
+            if (array1.GetLength(0) > array2.GetLength(0))
             {
-                for (int j = 0; j < arraySumm.GetLength(1); j++)
+                arraySum = new int[array1.GetLength(0), array1.GetLength(1)];
+                arraySizeMore = array1;
+                arraySizeLess = array2;
+            }
+
+            else 
+            {
+                arraySum = new int[array2.GetLength(0), array2.GetLength(1)];
+                arraySizeMore = array2;
+                arraySizeLess = array1;
+            }
+            
+            for (int i = 0; i < arraySum.GetLength(0); i++)
+            {
+                for (int j = 0; j < arraySum.GetLength(1); j++)
                 {
                     if (i < arraySizeLess.GetLength(0) && j < arraySizeLess.GetLength(1))
                     {
-                        arraySumm[i,j] = arraySizeMore[i,j] + arraySizeLess[i,j];
+                        arraySum[i, j] = arraySizeMore[i, j] + arraySizeLess[i, j];
                     }
                     else
                     {
-                        arraySumm[i, j] = arraySizeMore[i, j];
+                        arraySum[i, j] = arraySizeMore[i, j];
                     }
 
-                    Console.Write($"{arraySumm[i, j]}\t");
+                    Console.Write($"{arraySum[i, j]}\t");
 
                 }
                 Console.WriteLine();
             }
+
+            return arraySum;
         }
 
 
@@ -55,27 +75,21 @@ namespace ConsoleApp1
 
             Random rnd = new Random();
 
-            int[,] arrayA = new int[a,a];
-            int[,] arrayB = new int[b,b];
+            int[,] arrayA = new int[a, a];
+            int[,] arrayB = new int[b, b];
             int[,] arraySum;
 
             //Filling matrices with random numbers.
             ArrayGeneric2(arrayA, rnd);
             ArrayGeneric2(arrayB, rnd);
-                   
-            if (a > b)
-            {
-                arraySum = new int[arrayA.GetLength(0), arrayA.GetLength(1)];
-                ArraySumm(arrayA, arrayB, arraySum);
-            }
 
-            else if (a < b)
-            {
-                arraySum = new int[arrayB.GetLength(0), arrayB.GetLength(1)];
-                ArraySumm(arrayB, arrayA, arraySum);
-            }
+            arraySum = ArraySumm(arrayA, arrayB);
 
-            else Console.WriteLine("Error.");
+
+
+
+
+
         }
     }
 }
