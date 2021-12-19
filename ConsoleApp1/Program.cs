2399b0dbcd483491f5f3a5137adef0d7231553e6
Alexey <AVProsvirnin@bk.ru>
2021-12-19 20:00:42 +0400

    Sort

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,280 +1,189 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace ConsoleApp1
 {
-
     class Program
     {
-        static int GetSize(string inquiry)
+        static void Summ(params int[] numbers)
         {
-            Console.WriteLine(inquiry);
-            return int.Parse(Console.ReadLine());
-        }
-
-        static void GetArray(int[,] array, Random rnd)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
+            int summ = 0;
+            foreach (var item in numbers)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
+                summ += item;
+            }
+            Console.WriteLine(summ);
+        }
+        static void Multiplicationtable()
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 0; j <= 9; j++)
                 {
-                    array[i, j] = rnd.Next(0, 10);
+                    Console.WriteLine($"{i} * {j} = {i * j}");
                 }
+                Console.WriteLine();
             }
-            PrintArray(array);
+        }
+        static void Summ(int a, int b)
+        {
+            Console.WriteLine($"summ int {a + b}");
         }
 
-        static int[,] ArraySumm1(int[,] array1, int[,] array2)
+        static void Summ(double a, double b)
         {
-            int[,] arraySum;
-            int[,] arraySizeMore;
-            int[,] arraySizeLess;
-
-            Console.WriteLine();
-
-            if (array1.GetLength(0) > array2.GetLength(0))
-            {
-                arraySum = new int[array1.GetLength(0), array1.GetLength(1)];
-                arraySizeMore = array1;
-                arraySizeLess = array2;
-            }
-
-            else 
-            {
-                arraySum = new int[array2.GetLength(0), array2.GetLength(1)];
-                arraySizeMore = array2;
-                arraySizeLess = array1;
-            }
-            
-            for (int i = 0; i < arraySum.GetLength(0); i++)
-            {
-                for (int j = 0; j < arraySum.GetLength(1); j++)
-                {
-                    if (i < arraySizeLess.GetLength(0) && j < arraySizeLess.GetLength(1))
-                    {
-                        arraySum[i, j] = arraySizeMore[i, j] + arraySizeLess[i, j];
-                    }
-                    else
-                    {
-                        arraySum[i, j] = arraySizeMore[i, j];
-                    }
-                }
-            }
-            return arraySum;
+            Console.WriteLine($"summ double {a + b}");
+        }
+        static void Summ(long a, long b)
+        {
+            Console.WriteLine($"summ long {a + b}");
+        }
+        static void Summ(string a, string b)
+        {
+            Console.WriteLine($"concat string {a + b}");
         }
 
-        static int[,] ArraySumm2(int[,] array1, int[,] array2)
+        static void ArrayGeneration(int[] array)
         {
-            int[,] arraySum;
-            
-            Console.WriteLine();
+            Random rnd = new Random();
 
-            if (array1.GetLength(0) < array2.GetLength(0) && array1.GetLength(1) < array2.GetLength(1))
+            for (int i = 0; i < array.Length; i++)
             {
-                arraySum = new int[array1.GetLength(0), array1.GetLength(1)];
-            }
+                array[i] = rnd.Next(0, 2);
 
-            else if (array1.GetLength(0) < array2.GetLength(0) && array1.GetLength(1) > array2.GetLength(1))
-            {
-                arraySum = new int[array1.GetLength(0), array2.GetLength(1)];
+                Console.Write(array[i] + " ");
             }
-
-            else if (array1.GetLength(0) > array2.GetLength(0) && array1.GetLength(1) < array2.GetLength(1))
-            {
-                arraySum = new int[array2.GetLength(0), array1.GetLength(1)];
-            }
-
-            else if (array1.GetLength(0) > array2.GetLength(0) && array1.GetLength(1) > array2.GetLength(1))
-            {
-                arraySum = new int[array2.GetLength(0), array2.GetLength(1)];
-            }
-
-            else
-            {
-                Console.WriteLine("Error!!!");
-                arraySum = new int[0, 0];
-                return arraySum;
-            }
-
-            for (int i = 0; i < arraySum.GetLength(0); i++)
-            {
-                for (int j = 0; j < arraySum.GetLength(1); j++)
-                {
-                    arraySum[i, j] = array1[i, j] + array2[i, j];
-                }
-            }
-            return arraySum;
         }
 
-        static int[,] ArraySumm3(int[,] array1, int[,] array2)
+        static void ArrayGeneration(int[,] mass)
         {
-            int[,] arraySum;
-
-            Console.WriteLine();
-
-            if (array1.GetLength(0) < array2.GetLength(0) && array1.GetLength(1) < array2.GetLength(1))
+            Random rnd = new Random();
+            for (int i = 0; i < mass.GetLength(0); i++)
             {
-                arraySum = new int[array2.GetLength(0), array2.GetLength(1)];
-            }
-
-            else if (array1.GetLength(0) < array2.GetLength(0) && array1.GetLength(1) > array2.GetLength(1))
-            {
-                arraySum = new int[array2.GetLength(0), array1.GetLength(1)];
-            }
-
-            else if (array1.GetLength(0) > array2.GetLength(0) && array1.GetLength(1) < array2.GetLength(1))
-            {
-                arraySum = new int[array1.GetLength(0), array2.GetLength(1)];
-            }
-
-            else if (array1.GetLength(0) > array2.GetLength(0) && array1.GetLength(1) > array2.GetLength(1))
-            {
-                arraySum = new int[array1.GetLength(0), array1.GetLength(1)];
-            }
-
-            else
-            {
-                Console.WriteLine("Error!!!");
-                arraySum = new int[0, 0];
-                return arraySum;
-            }
-
-            for (int i = 0; i < arraySum.GetLength(0); i++)
-            {
-                for (int j = 0; j < arraySum.GetLength(1); j++)
+                for (int j = 0; j < mass.GetLength(1); j++)
                 {
-                    if (i < array1.GetLength(0) && i < array2.GetLength(0) && j < array1.GetLength(0) && j < array2.GetLength(0))
-                    {
-                        arraySum[i, j] = array1[i, j] + array2[i, j];
-                    }
 
-                    else
-                    {
-                        arraySum[i, j] = 0;
-                    }
-                    
-                }
-            }
-            return arraySum;
-        }
+                    mass[i, j] = rnd.Next(0, 2);
 
-        static void PrintArray(int [,] array)
-        {
-            Console.WriteLine();
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    Console.Write($"{array[i, j]}\t");
+                    Console.Write(mass[i, j] + " ");
                 }
                 Console.WriteLine();
             }
         }
 
+        static void ArrayGeneration(int[] array, int n)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(n);
+
+                Console.Write(array[i] + " ");
+            }
+        }
+
+        static void ArrayGeneration(int[,] mass, int n)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+
+                    mass[i, j] = rnd.Next(n);
+
+                    Console.Write(mass[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void ArrayGeneration(int[] array, int n, int m)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(n, m);
+
+                Console.Write(array[i] + " ");
+            }
+        }
+
+        static void ArrayGeneration(int[,] mass, int n, int m)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+
+                    mass[i, j] = rnd.Next(n, m);
+
+                    Console.Write(mass[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void ArraySorting(int[] array)
+        {
+            for (int j = 0; j < array.Length - 1; j++)
+            {
+                for (int i = 0; i < array.Length - 1 - j; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        int buffer = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = buffer;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in array)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+
+
+        static void ArraySorting(int[] array, bool a)
+        {
+            for (int j = 0; j < array.Length - 1; j++)
+            {
+                for (int i = 0; i < array.Length - 1 - j; i++)
+                {
+                    if (array[i] < array[i + 1])
+                    {
+                        int buffer = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = buffer;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in array)
+            {
+                Console.Write($"{item} ");
+            }
+        }
 
         static void Main(string[] args)
         {
-            bool key = true;
-
-            int sizeA;
-            int sizeB;
-
-            int sizeA1;
-            int sizeA2;
-            int sizeB1;
-            int sizeB2;
-
-            int[,] arrayA;
-            int[,] arrayB;
-
-            Random rnd = new Random();
-
-            int[,] arraySum;
-
-            
-
-            while (key)
-            {
-                Console.WriteLine();
-                Console.WriteLine("Select job number or exit 1/ 2/ 3 or 0:");
-                int jobNumber = int.Parse(Console.ReadLine());
-
-                switch (jobNumber)
-                {
-                    case 0:
-                        key = false;
-                        break;
-
-                    case 1:
-                        sizeA = GetSize("Enter the size of the first matrix:");
-                        sizeB = GetSize("Enter the size of the second matrix:");
-
-                        arrayA = new int[sizeA, sizeA];
-                        GetArray(arrayA, rnd);
-
-                        arrayB = new int[sizeB, sizeB];
-                        GetArray(arrayB, rnd);
-
-                        arraySum = ArraySumm1(arrayA, arrayB);
-
-                        PrintArray(arraySum);
-                       
-                        break;
-
-                    case 2:
-                        sizeA1 = GetSize("Enter the size one of the first array:");
-                        sizeA2 = GetSize("Enter the size two of the first array:");
-                        sizeB1 = GetSize("Enter the size one of the second array:");
-                        sizeB2 = GetSize("Enter the size two of the second array:");
-
-                        arrayA = new int[sizeA1, sizeA2];
-                        GetArray(arrayA, rnd);
-
-                        arrayB = new int[sizeB1, sizeB2];
-                        GetArray(arrayB, rnd);
-
-                        arraySum = ArraySumm2(arrayA, arrayB);
-
-                        PrintArray(arraySum);
-
-                        break;
-
-                    case 3:
-                        sizeA1 = GetSize("Enter the size one of the first array:");
-                        sizeA2 = GetSize("Enter the size two of the first array:");
-                        sizeB1 = GetSize("Enter the size one of the second array:");
-                        sizeB2 = GetSize("Enter the size two of the second array:");
-
-                        arrayA = new int[sizeA1, sizeA2];
-                        GetArray(arrayA, rnd);
-
-                        arrayB = new int[sizeB1, sizeB2];
-                        GetArray(arrayB, rnd);
-
-                        arraySum = ArraySumm3(arrayA, arrayB);
-
-                        PrintArray(arraySum);
-
-                        break;
-
-                    default:
-                        Console.WriteLine("You entered the wrong job number!");
-                        break;
-                }
-            }
-
-            
-
-
-
-
-            
-
-
-
-
-
+            int[] array = new int[10];
+            ArrayGeneration(array, 100);
+            ArraySorting(array);
+            ArraySorting(array, true);
 
         }
+
+
     }
 }
