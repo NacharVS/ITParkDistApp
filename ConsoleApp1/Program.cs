68363fbe40e61bacd5ec656a9d30b574f8d694b3
DESKTOP-U06RGGG\Alberttos <albertto09@bk.ru>
2021-12-19 21:20:43 +0300

    HomeWork 3(Magaz)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,44 +10,70 @@ namespace ConsoleApp1
         {
             //Задача № 1
 
-            //List<string> products = new List<string>() { "Сахар", "Мандарины" };
-            //List<double> prices = new List<double>() { 50, 50 };
-            //Console.WriteLine("Client/Admin");
-
-            //string s = Console.ReadLine();
-
-            //if (s == "c")
-            //{
-            //    Console.WriteLine("Введите сумму");
-            //    double money = 2000;
-            //    while (true)
-            //    {
-            //        Console.WriteLine("our list");
-            //        for (int i = 0; i < products.Count; i++)
-            //        {
-            //            Console.WriteLine($"{i + 1}.{products[i]}{prices[i]}");
-            //        }
-            //        Console.WriteLine("Make Ur choice(1,2,3...):");
-            //        string clientsChoice = Console.ReadLine();
+            List<string> products = new List<string>() { "Сахар-", "Молоко-","Хлеб-"};
+            List<double> prices = new List<double>() { 50, 50, 50 };
 
 
-            //        Console.WriteLine("something else?(Y/N)");
-            //        clientsChoice = Console.ReadLine();
-            //        if (clientsChoice == "N")
-            //        {
-            //            break;
-            //        }
-            //    }
-            //}
+            List<double> cart= new List<double>() { 50, 50, 50 };
+            Console.WriteLine("Client/Admin");
 
-            
+            string s = Console.ReadLine();
+
+            if (s == "c")
+            {
+
+                Console.WriteLine("Вы зашли как клиент");
+                Console.WriteLine("Введите сумму");
+                //double money =  2000;
+                while (true)
+                {
+                    Console.WriteLine("Ваш список");
+                    for (int i = 0; i < products.Count; i++)
+                    {
+                        Console.WriteLine($"{i + 1}.{products[i]}{prices[i]}");
+                    }
+                    Console.Write("Выберите товар:(1,2,3...):");
+                    string clientsChoice = Console.ReadLine();
+
+                    switch (clientsChoice)
+                    {
+                        case "1":
+                            {
+                                Console.Write("Введите кол-во:");
+                                int countProduct = int.Parse(Console.ReadLine());
+                                cart.Add(prices[0] * countProduct);
+                            }
+                            break;
+
+                    }
+                    Console.WriteLine("Хотите купить еще E(Y/N)");
+                    clientsChoice = Console.ReadLine();
+                    if (clientsChoice == "N")
+                    {
+                        break;
+                    }
+                    
+                }
+                double SUMM = 0;
+                for (int i = 0; i <= cart.Count; i++)
+                {
+                    SUMM += cart[i];
+                }
+                Console.WriteLine(SUMM);
+            }
+            else
+            {
+                Console.WriteLine("Вы зашли как админ");
+            }
 
 
-            
 
 
 
-            
+
+
+
+
         }
 
 
