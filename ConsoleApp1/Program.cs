f107ae1b28effc006bb085214b8916a56be4cb22
NacharVS <r3dinc@yandex.ru>
2021-12-19 20:32:47 +0300

    hw
    home work

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,9 +8,25 @@ namespace ConsoleApp1
     {        
         static void Main(string[] args)
         {
-            
+            int[,] matrix = new int[10, 10];
+            ArrayOperations.ArrayGeneration(matrix);
+
+            SummID[] summIDs = new SummID[10];
+            for (int i = 0; i < summIDs.Length; i++)
+            {
+                summIDs[i] = new SummID();             
+            }
+
+            // 1. Отсортировать номера строк по возрастанию сумм в кажджой строке матрицы.
+            // 2. Переделать Магазин из ДЗ2 через классы. 
+            // * Все задания выполняем отдельных файлах
+
+
         }
-        
+        // 1 2 3 - 6      1 3 2
+        // 4 4 6 - 14
+        // 7 1 2 - 10
+
 
     }
 }
