3167a6352b4dc49a1dbda3cd61a643073a8ca6e4
daryazakharova <daryazakharova87@gmail.com>
2021-12-19 19:52:48 +0300

    Class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,65 +5,16 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+       
+            
+static void Main(string[] args)
         {
+
             
-
-             int[,] array1 = new int [2, 5];
-             Random rnd = new();
-             int[,] array2 = new int[5, 3];
-             Random rnd2 = new();
-
-
-
-             for (int i = 0; i < array1.GetLength(0); i++)
-             {
-                 for (int j = 0; j < array1.GetLength(1); j++)
-                 {
-                     array1[i, j] = rnd.Next(0,10);
-                     Console.Write($"{array1[i,j]} ");
-                 }
-                 Console.WriteLine();
-             }
-             Console.WriteLine();
-
-
-
-             for (int i = 0; i < array2.GetLength(0); i++)
-             {
-                 for (int j = 0; j < array2.GetLength(1); j++)
-                 {
-                     array2[i, j] = rnd2.Next(0, 10);
-                     Console.Write($"{array2[i, j]} ");
-                 }
-                 Console.WriteLine();
-             }
-             Console.WriteLine();
-
-             
-
-
-             int[,] array3 = new int[2, 3];
-             for (int i = 0; i < array3.GetLength(0); i++)
-             {
-                 for (int j = 0; j < array3.GetLength(1); j++)
-                 {
-                     array3[i, j] = array1[i, j] + array2[i, j];
-                     Console.Write($"{array3[i, j]} ");
-                 }
-                 Console.WriteLine();
-             }
-             Console.WriteLine();
-            
-            
-
-
-
-
         }
-           
-            
-     }
+
+
+    }
 
 }
   
---
+++ ConsoleApp1/hw5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class hw5
+    {
+        
+        static void Summ(int a, int b)
+        {
+            Console.WriteLine($"summ int {a + b}");
+        }
+
+        static void Summ(double a, double b)
+        {
+            Console.WriteLine($"summ double {a + b}");
+        }
+        static void Summ(long a, long b)
+        {
+            Console.WriteLine($"summ long {a + b}");
+        }
+        static void Summ(string a, string b)
+        {
+            Console.WriteLine($"concat string {a + b}");
+
+
+        }
+
+    }
+}
