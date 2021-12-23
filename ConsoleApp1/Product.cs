a84c55549a570cb8d2693312a96a31ab2a883512
Petrikovnl <petrikovnl@gmail.com>
2021-12-23 22:09:16 +0500

    Lesson7

---
+++ ConsoleApp1/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Product
+    {
+        public string productName;
+        public double price;
+        public double countOfProduct;
+        public string shop;
+        public double 
+        
+        public Product(string productName, double price, double countOfProduct)
+        {
+            productName = productName;
+            price = price;
+            countOfProduct = countOfProduct;
+            shop = "Albatros";
+        }
+        public Product()
+        {
+
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,15 +8,70 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[,] matrix = new int[10, 10];
+            List<Product>products=new List<Product>();
+            List<Product> cart = new List<Product>();
+            string name = Console.ReadLine();
+            double count = int.Parse(Console.ReadLine());
+
+            Product bread = new Product("borodinsky hleb",40,80);
+            Product beans = new Product("fasol",45.45,8.7);
+            //Console.WriteLine($"{bread.shop} {beans.shop}");
+            products.Add(bread);
+            products.Add(beans);
+            products.Add(new Product("Tvorog", 50, 40));
+            products.Add(new Product("Yogurt", 50, 40));
+            products.Add(new Product("Cheese", 50, 40));
+            products.Add(new Product(name, adminPrice, count));
+            foreach (var item in products)
+            {
+                Console.WriteLine($"{item.productName} {item.price} ");
+            }
+            string neededProd = Console.ReadLine();
+            foreach (var item in products)
+            {
+                if(item.productName==neededProd)
+                {
+                    cart.Add(item);
+                }
+            }
+
+
+            //cart.Add(products.Find(x => x.productName == "fasol"));
+            /*int[,] matrix = new int[10, 10];
             ArrayOperations.ArrayGeneration(matrix);
 
-            /*SummID[] summIDs = new SummID[10];
+            SummID[] summIDs = new SummID[10];
             for (int i = 0; i < summIDs.Length; i++)
             {
+                
+            }
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
                 summIDs[i] = new SummID();
+                summIDs[i].rowId = i + 1;
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    summIDs[i].summInCurrentRow += matrix[i, j];
+                }
+            }
+            foreach (var item in summIDs)
+            {
+                Console.WriteLine($"{item.rowId}) {item.summInCurrentRow}");
+            }
+            for (int i = 0; i < summIDs.Length-1; i++)
+            {
+                for (int j = 0; j < summIDs.Length-1-i; j++)
+                {
+                    int buff1 = 0;
+                    int buff2;
+                    if (summIDs[j].summInCurrentRow > summIDs[j + 1].summInCurrentRow)
+                    { 
+                        buff1=summIDs.     
+                            
+                    }
+                }
             }*/
-            int[] arrayOfMatrixSumRow = new int[10];
+            /*int[] arrayOfMatrixSumRow = new int[10];
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 int sum = 0;
@@ -30,7 +85,7 @@ namespace ConsoleApp1
             Console.WriteLine("Сумма строк:");
             ArrayOperations.ArrayPrint(arrayOfMatrixSumRow);
             Console.WriteLine("Отсортированный массив сумм:");
-            ArrayOperations.ArraySorting(arrayOfMatrixSumRow);
+            ArrayOperations.ArraySorting(arrayOfMatrixSumRow);*/
         }
     }
 }
