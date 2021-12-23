13ca9ae5765f36fea2e0d9bbe2b30ef50aee2273
nacharvs <r3dinc@yandex.ru>
2021-12-23 20:18:51 +0300

    sumId solution

---
+++ ConsoleApp1/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Product
+    {
+        public string productName;
+        public double price;
+        public double countOfProduct;
+        public string currentShop;
+
+        public Product(string productName, double price, double countOfProduct)
+        {
+            this.productName = productName;
+            this.price = price;
+            this.countOfProduct = countOfProduct;
+            currentShop = "Albatros";
+        }
+
+        public Product(string productName, double price, double countOfProduct, string shop)
+        {
+            this.productName = productName;
+            this.price = price;
+            this.countOfProduct = countOfProduct;
+            currentShop = shop;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
@@ -7,51 +7,98 @@ namespace ConsoleApp1
     {        
         static void Main(string[] args)
         {
-            int[,] matrix = new int[10, 10];
-            ArrayOperations.ArrayGeneration(matrix, 100);
-
-            SummID[] summIDs = new SummID[10];
-
-
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            List<Product> products = new List<Product>();
+            List<Product> cart = new List<Product>();
+            Product bread = new Product("бородинский хлеб", 40, 80);
+            Product beans = new Product("фасоль", 45.45, 8.7);
+            products.Add(bread);
+            products.Add(beans);
+            products.Add(new Product("Творог", 50, 40));
+            products.Add(new Product("Йогурт", 50, 40));
+            products.Add(new Product("Сыр", 50, 40));
+            while (true)
             {
-                summIDs[i] = new SummID();
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                string name = Console.ReadLine();
+                double adminPrice = int.Parse(Console.ReadLine());
+                double count = int.Parse(Console.ReadLine());
+
+
+                products.Add(new Product(name, adminPrice, count));
+
+                foreach (var item in products)
                 {
-                    summIDs[i].rowId = i;
-                    summIDs[i].summInCurrentRow += matrix[i, j];
+                    Console.WriteLine($"{item.productName} {item.price}");
                 }
+                Console.WriteLine("y/n");
+                string n = Console.ReadLine();
+                if (n == "n")
+                    break;
             }
-            Console.WriteLine();
-            foreach (var item in summIDs)
-            {
-                Console.WriteLine($"{item.rowId + 1} - {item.summInCurrentRow}");
-            }
+            //string neededProd = Console.ReadLine();
 
-            for (int i = 0; i < summIDs.Length- 1; i++)
-            {
-                for (int j = 0; j < summIDs.Length - 1 - i; j++)
-                {
-                    int buff1 = 0;
-                    int buff2 = 0;
-                    if(summIDs[j].summInCurrentRow > summIDs[j + 1].summInCurrentRow)
-                    {
-                        buff1 = summIDs[j].rowId;
-                        buff2 = summIDs[j].summInCurrentRow;
-                        summIDs[j].summInCurrentRow = summIDs[j + 1].summInCurrentRow;
-                        summIDs[j].rowId = summIDs[j + 1].rowId;
-                        summIDs[j + 1].summInCurrentRow = buff2;
-                        summIDs[j + 1].rowId = buff1;
+            //foreach (var item in products)
+            //{
+            //   if(item.productName == neededProd)
+            //   {
+            //        cart.Add(item);
+            //   }
+            //}
 
-                    }
 
-                }
-            }
-            Console.WriteLine();
-            foreach (var item in summIDs)
-            {
-                Console.WriteLine($"{item.rowId + 1} - {item.summInCurrentRow}");
-            }
+
+            //cart.Add(products.Find(x => x.productName == "фасоль"));
+
+
+
+
+
+
+            //int[,] matrix = new int[10, 10];
+            //ArrayOperations.ArrayGeneration(matrix, 100);
+
+            //SummID[] summIDs = new SummID[10];
+
+
+            //for (int i = 0; i < matrix.GetLength(0); i++)
+            //{
+            //    summIDs[i] = new SummID();
+
+            //    for (int j = 0; j < matrix.GetLength(1); j++)
+            //    {
+            //        summIDs[i].rowId = i;
+            //        summIDs[i].summInCurrentRow += matrix[i, j];
+            //    }
+            //}
+            //Console.WriteLine();
+            //foreach (var item in summIDs)
+            //{
+            //    Console.WriteLine($"{item.rowId + 1} - {item.summInCurrentRow}");
+            //}
+
+            //for (int i = 0; i < summIDs.Length- 1; i++)
+            //{
+            //    for (int j = 0; j < summIDs.Length - 1 - i; j++)
+            //    {
+            //        int buff1 = 0;
+            //        int buff2 = 0;
+            //        if(summIDs[j].summInCurrentRow > summIDs[j + 1].summInCurrentRow)
+            //        {
+            //            buff1 = summIDs[j].rowId;
+            //            buff2 = summIDs[j].summInCurrentRow;
+            //            summIDs[j].summInCurrentRow = summIDs[j + 1].summInCurrentRow;
+            //            summIDs[j].rowId = summIDs[j + 1].rowId;
+            //            summIDs[j + 1].summInCurrentRow = buff2;
+            //            summIDs[j + 1].rowId = buff1;
+
+            //        }
+
+            //    }
+            //}
+            //Console.WriteLine();
+            //foreach (var item in summIDs)
+            //{
+            //    Console.WriteLine($"{item.rowId + 1} - {item.summInCurrentRow}");
+            //}
 
             // 1. Отсортировать номера строк по возрастанию сумм в кажджой строке матрицы.
             // 2. Переделать Магазин из ДЗ2 через классы. 
