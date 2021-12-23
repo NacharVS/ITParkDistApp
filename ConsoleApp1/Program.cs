819eac030b25edc60f9583b6ffdc7a1cab26a668
nacharvs <r3dinc@yandex.ru>
2021-12-23 19:16:26 +0300

    sumId solution

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Threading;
+
 
 namespace ConsoleApp1
 {
@@ -9,12 +8,49 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int[,] matrix = new int[10, 10];
-            ArrayOperations.ArrayGeneration(matrix);
+            ArrayOperations.ArrayGeneration(matrix, 100);
 
             SummID[] summIDs = new SummID[10];
-            for (int i = 0; i < summIDs.Length; i++)
+
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                summIDs[i] = new SummID();             
+                summIDs[i] = new SummID();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    summIDs[i].rowId = i;
+                    summIDs[i].summInCurrentRow += matrix[i, j];
+                }
+            }
+            Console.WriteLine();
+            foreach (var item in summIDs)
+            {
+                Console.WriteLine($"{item.rowId + 1} - {item.summInCurrentRow}");
+            }
+
+            for (int i = 0; i < summIDs.Length- 1; i++)
+            {
+                for (int j = 0; j < summIDs.Length - 1 - i; j++)
+                {
+                    int buff1 = 0;
+                    int buff2 = 0;
+                    if(summIDs[j].summInCurrentRow > summIDs[j + 1].summInCurrentRow)
+                    {
+                        buff1 = summIDs[j].rowId;
+                        buff2 = summIDs[j].summInCurrentRow;
+                        summIDs[j].summInCurrentRow = summIDs[j + 1].summInCurrentRow;
+                        summIDs[j].rowId = summIDs[j + 1].rowId;
+                        summIDs[j + 1].summInCurrentRow = buff2;
+                        summIDs[j + 1].rowId = buff1;
+
+                    }
+
+                }
+            }
+            Console.WriteLine();
+            foreach (var item in summIDs)
+            {
+                Console.WriteLine($"{item.rowId + 1} - {item.summInCurrentRow}");
             }
 
             // 1. Отсортировать номера строк по возрастанию сумм в кажджой строке матрицы.
--- ConsoleApp1/SummID.cs
+++ ConsoleApp1/SummID.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
     class SummID
     {
         public int rowId;
-        public int summInCurrentrow;
+        public int summInCurrentRow;
     }
 }
