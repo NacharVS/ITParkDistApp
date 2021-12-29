3a2acdf1604fcfa9146370527a8695fd7b2b1ed0
Petrikovnl <petrikovnl@gmail.com>
2021-12-30 00:36:48 +0500

    Lesson_8

--- ConsoleApp1/Strategy/Archer.cs
+++ ConsoleApp1/Strategy/Archer.cs
@@ -4,25 +4,25 @@ namespace ConsoleApp1.Strategy
 {
     class Archer : BattleUnit
     {
-        int arrows;
-        int minRangeDamage;
-        int maxRangeDamage;
+        private int _arrows;
+        private int _minRangeDamage;
+        private int _maxRangeDamage;
 
         public Archer(string name) : base(name, 45, 0, "shooter", 7, 1, 2)
         {
-            arrows = 5;
-            minRangeDamage = 4;
-            maxRangeDamage = 12;
+            _arrows = 5;
+            _minRangeDamage = 4;
+            _maxRangeDamage = 12;
         }
 
         public void RangeAttack(Peasant peasant, Random rnd)
         {
-            if (arrows > 0)
+            if (_arrows > 0)
             {
-                double currentDamage = Convert.ToDouble(rnd.Next(minRangeDamage, maxRangeDamage));
+                double currentDamage = Convert.ToDouble(rnd.Next(_minRangeDamage, _maxRangeDamage));
                 peasant.health -= currentDamage;
                 Console.WriteLine($"{name} attack {peasant.name} with {currentDamage} points of damage");
-                arrows--;
+                _arrows--;
                 if (peasant.health <= 0)
                     peasant.Termination();
             }
