aaa9b2f819272c1eee58d7b2aec4586cac4b654d
DESKTOP-F8PTRBK\Нияз <kamalov.niaz@mail.ru>
2022-01-12 01:01:43 +0300

    Home work "market" продолжение

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,26 +12,101 @@ namespace ConsoleApp1
         {
 
 
-            List<string> products = new List<string>() { "Beear", "Vodka", "Seledka" };
-            List<double> prices = new List<double>() { 100, 450, 250 };
-            List<string> currency_name = new List<string>() { "Р", "Р", "Р" };
+            List<string> products = new List<string>() { "Beer", "Vodka", "Seledka", "Lavash" , "Juice" };
+            List<double> prices = new List<double>() { 100, 450, 250, 50, 190 };
+            List<string> currency_name = new List<string>() { "Р", "Р", "Р" , "Р" , "Р" };
+
+            List<double> shopping_basket = new List<double>() { 100, 450, 250 };
+
             Console.WriteLine();
-            Console.Write("Client and Admin:");
+            Console.Write("Client and Admin:  ");
             string s = Console.ReadLine();
            
             if (s == "Client")
             {
                 while (true)
                 {
-                    Console.WriteLine("Enter the amount of money");
-                    double money = 2500;
+                    Console.Write("Your credit limit: ");
+                    double credit = double.Parse(Console.ReadLine());
+
                     Console.WriteLine("Spicok");
                     for (int i = 0; i < products.Count; i++)
                     {
                         Console.WriteLine($"{i + 1}. {products[i]} {prices[i]} {currency_name[i]}");
 
                     }
-                    Console.WriteLine("make your choice(ot 1 do 3)");
+                    Console.WriteLine("Make your choice(ot 1 do 5)");
+                    string client_choise = Console.ReadLine();
+                    switch (client_choise)
+                    {
+                        case "1":
+                            {
+                                Console.WriteLine("Enter coli. Beer");
+                                int coli_product = int.Parse(Console.ReadLine());
+                                if (prices[0] * coli_product > credit)
+                                {
+                                    Console.WriteLine("Error Credit limit");
+                                }
+                                shopping_basket.Add (prices[0] * coli_product); 
+                            }
+                            break;
+
+                        case "2":
+                            {
+                                Console.WriteLine("Enter coli. Vodka");
+                                int coli_product = int.Parse(Console.ReadLine());
+                                if (prices[1] * coli_product > credit)
+                                {
+                                    Console.WriteLine("Error Credit limit");
+                                }
+                                shopping_basket.Add(prices[1] * coli_product);
+                            }
+                            break;
+
+                        case "3":
+                            {
+                                Console.WriteLine("Enter coli. Seledka");
+                                int coli_product = int.Parse(Console.ReadLine());
+                                if (prices[2] * coli_product > credit)
+                                {
+                                    Console.WriteLine("Error Credit limit");
+                                }
+                                shopping_basket.Add(prices[2] * coli_product);
+                            }
+                            break;
+
+                        case "4":
+                            {
+                                Console.WriteLine("Enter coli. Lavash");
+                                int coli_product = int.Parse(Console.ReadLine());
+                                if (prices[3] * coli_product > credit)
+                                {
+                                    Console.WriteLine("Error Credit limit");
+                                }
+                                shopping_basket.Add(prices[3] * coli_product);
+                            }
+                            break;
+
+                        case "5":
+                            {
+                                Console.WriteLine("Enter coli. Juice");
+                                int coli_product = int.Parse(Console.ReadLine());
+                                if (prices[4] * coli_product > credit)
+                                {
+                                    Console.WriteLine("Error Credit limit");
+                                }
+                                shopping_basket.Add(prices[4] * coli_product);
+                            }
+                            break;
+                    }
+                    Console.Write("Anything else? (Y/N)");
+                    client_choise = Console.ReadLine();
+                    if (client_choise == "N")
+                    {
+                        break;
+                    }
+                        
+
                 }
                 
             }
@@ -51,72 +126,7 @@ namespace ConsoleApp1
             }
             
 
-            /*ДЗ №2 задание 2
 
-            Console.Write("Введите число");
-
-            int num = int.Parse(Console.ReadLine());
-
-
-            while (num > 1 && num % 2 == 0)
-            {
-                Console.WriteLine(2);
-                num = num / 2;
-            }
-
-            int a = 3;
-            while (num > 1)
-            {
-                while (num % a == 0)
-                {
-                    Console.WriteLine(a);
-                    num = num / a;
-                }
-                a = a + 2;
-            }*/
-
-            /*ДЗ №2 задание 1
-
-            int sum = int.Parse(Console.ReadLine());
-            int combin = 0;
-            for (int i = 0; i <= 9; i++)
-            {
-                for (int k = 0; k <= 9; k++)
-                {
-                    for (int l = 0; l <= 9; l++)
-                    {
-                        if (i + k + l == sum && i != k && k != l && i != l)
-                        {
-                            Console.WriteLine($"{i}{k}{l}");
-                            combin++;
-                        }
-                    }
-                }
-            }
-            Console.WriteLine("Комбинаций=" + combin);
-            */
-
-            /*ДЗ №2 задание 1
-
-            int n = int.Parse(Console.ReadLine());
-            int max = 0;
-            int maxsum = 0;
-            for (int i = 1; i <=n; i++)
-            {
-                int a = i;
-                int sum = 0;
-                while (a>0)
-                {
-                    sum += a % 10;
-
-                    a = a / 10;
-                }
-                if (maxsum < sum)
-                {
-                    maxsum = sum;
-                    max = i;
-                }*/
-        }
         }
     }
 
