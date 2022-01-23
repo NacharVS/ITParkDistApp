939c5f2b087495cf5859702ddb6df812db39553b
Alexey <AVProsvirnin@bk.ru>
2022-01-23 15:05:57 +0400

    Delegate

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
-using ConsoleApp1.Mylib;
-using ConsoleApp1.Strategy;
-
-
 
 namespace ConsoleApp1
 {
@@ -12,78 +8,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
+            StockExchangeMonitor stockExchangeMonitor = new StockExchangeMonitor();
 
-            //WathingTower wathingTower = new WathingTower("WathingTower");
-            Catapult catapult = new Catapult();
+            stockExchangeMonitor.PriceChangeHandler = ShowPrice;
 
-            //ArcherTower archerTower = new ArcherTower();
-
-            Healer healer = new Healer("Gendalf");
-
-            Warrior warrior1 = new Warrior("Bob1");
-            Warrior warrior2 = new Warrior("Bob2");
-
-            //Shaman shaman = new Shaman("Gendalf");
-
-            //shaman.StoneSkin(warrior1);
-            //shaman.Frenzy(warrior2);
-
-            //Archer archer1 = new Archer("Legolaz1");
-            //Archer archer2 = new Archer("Legolaz2");
-            healer.SalvationBuff(warrior1);
-            healer.SalvationBuff(warrior1);
-
-            warrior1.HealthChangedEvent += Message;
-            
-
-            Battle.Fight(warrior1, catapult);
-            //healer1.Treatment(warrior1, rnd);
-            //healer1.Treatment(warrior2, rnd);
-
-            //Battle.Fight(archer1, archer2);
-            //healer1.Treatment(archer1, rnd);
-            //healer1.Treatment(archer2, rnd);
-
-            //Battle.Fight(archer1, warrior1);
-            //healer1.Treatment(archer1, rnd);
-            //healer1.Treatment(warrior1, rnd);
-
-            //Battle.Fight(archer2, warrior2);
-            //healer1.Treatment(archer2, rnd);
-            //healer1.Treatment(warrior2, rnd);
-
-            //Battle.BuildingAttack(wathingTower, catapult);
-            //healer1.Treatment(archer1, rnd);
-            //healer1.Treatment(warrior1, rnd);
-
-            //Battle.Fight(warrior1, catapult);
-            //healer1.Treatment(warrior1, rnd);
-            //healer1.Treatment(catapult, rnd);
-
-            //Battle.MovingAttack(catapult, warrior1);
-            //healer1.Treatment(warrior1, rnd);
-            //healer1.Treatment(catapult, rnd);
-
-            //Battle.Fight(archerTower, warrior1);
-            //healer1.Treatment(warrior1, rnd);
-            //healer1.Treatment(catapult, rnd);
-
-            void Message()
-            {
-                Console.WriteLine($"Health has changed.");
-            }
-
-            
-
-            // Реализовать новый юнит Shaman которыйможет накидывать ряд усилений. 
-            // 1. BoostHealth - Увеличивает максимальное здоровье на 50%
-            // 2. Frenzy - Увеличивает показатель урона на 1,5 и снижает показатель защиты 0.5
-            // 3. StoneSkin - увеличивает показатель защиты в 2 раза, и снижает скорость в 2 раза
-            // *все усиления могут накладывать только 1 раз на 1 юнита (не стакаються)
-
+            stockExchangeMonitor.Start();
         }
 
-
+        public static void ShowPrice(int price)
+        {
+            Console.WriteLine($"New price: {price}");
+        }
     }
 }
---
+++ ConsoleApp1/StockExchangeMonitor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class StockExchangeMonitor
+    {
+        public delegate void PriceChange(int price);
+
+        public PriceChange _priceChangeHandler;
+
+        public PriceChange PriceChangeHandler { get => _priceChangeHandler; set => _priceChangeHandler = value; }
+
+        public void Start()
+        {
+            while (true)
+            {
+                int bankOfAmericaPrice = (new Random()).Next(100);
+
+                PriceChangeHandler(bankOfAmericaPrice);
+
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
