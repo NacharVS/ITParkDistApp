6a5022aa8d0c5059ae0812765993d752f9aae657
Petrikov Nikolay <petrikovnl@gmail.com>
2022-01-30 22:01:47 +0500

    13 lection

---
+++ ConsoleApp1/InterfacesLab/Pistol.cs
@@ -0,0 +1,13 @@
+﻿using PowerArgs.Games;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.InterfacesLab
+{
+    internal class Pistol : Weapon
+    {
+    }
+}
